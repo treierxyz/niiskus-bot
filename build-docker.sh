566cc55cfd6a700454283faa0707818a9f49e037
treierxyz <me@treier.xyz>
2023-11-18 00:15:39 +0200

    Deprecating build script in favor of GH Action

--- build-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-IMAGE_NAME="treierxyz/niiskus-bot"
-BUILDER_NAME="niiskus-bot-builder"
-
-mkdir -p build
-docker buildx create --use --name "$BUILDER_NAME"
-docker buildx build -t $IMAGE_NAME -o type=docker,dest=build/niiskus-bot.tar .
-docker buildx rm "$BUILDER_NAME"
